cd818687ac4ba6ccb20fa12ed30136f5601b9cc4
Heng Li <lh3@me.com>
2012-04-17 20:43:43 -0400

    r115: added -I and -S to bwasw

--- bwtsw2_aux.c
+++ bwtsw2_aux.c
@@ -50,7 +50,8 @@ bsw2opt_t *bsw2_init_opt()
 	bsw2opt_t *o = (bsw2opt_t*)calloc(1, sizeof(bsw2opt_t));
 	o->a = 1; o->b = 3; o->q = 5; o->r = 2; o->t = 30;
 	o->bw = 50;
-	o->z = 1; o->is = 3; o->t_seeds = 5; o->hard_clip = 0;
+	o->max_ins = 20000;
+	o->z = 1; o->is = 3; o->t_seeds = 5; o->hard_clip = 0; o->skip_sw = 0;
 	o->mask_level = 0.50f; o->coef = 5.5f;
 	o->qr = o->q + o->r; o->n_threads = 1; o->chunk_size = 10000000;
 	return o;
