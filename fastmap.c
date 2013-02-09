cb55617f50ca20cf231bf4012dca5dee26715091
Heng Li <lh3@me.com>
2013-02-08 22:12:18 -0500

    added a new line

--- fastmap.c
+++ fastmap.c
@@ -33,7 +33,7 @@ int main_mem(int argc, char *argv[])
 	}
 	if (optind + 1 >= argc) {
 		fprintf(stderr, "\n");
-		fprintf(stderr, "Usage:   bwa mem [options] <idxbase> <in.fq>\n");
+		fprintf(stderr, "Usage:   bwa mem [options] <idxbase> <in.fq>\n\n");
 		fprintf(stderr, "Options: -k INT     minimum seed length [%d]\n", opt->min_seed_len);
 		fprintf(stderr, "         -c INT     skip seeds with more than INT occurrences [%d]\n", opt->max_occ);
 		fprintf(stderr, "         -s INT     look for internal seeds inside a seed with less than INT occ [%d]\n", opt->split_width);
