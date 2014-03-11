81c4d369b2bd2563b5a6d67148399f31792319a3
Pascal Costanza <pascal.costanza@intel.com>
2014-03-11 12:56:02 +0100

    Added optional aligned malloc / calloc.

--- malloc_wrap.c
+++ malloc_wrap.c
@@ -8,9 +8,27 @@
 #endif
 #include "malloc_wrap.h"
 
+void *align_calloc(size_t nmemb, size_t size) {
+  size_t n = nmemb*size;
+  void *p;
+  if (posix_memalign(&p,64,n)) p=NULL;
+  else memset(p,0,n);
+  return p;
+}
+
+void *align_malloc(size_t size) {
+  void *p;
+  if (posix_memalign(&p,64,size)) p=NULL;
+  return p;
+}
+
 void *wrap_calloc(size_t nmemb, size_t size,
 				  const char *file, unsigned int line, const char *func) {
+#ifdef USE_ALIGNED_MALLOC
+  void *p = align_calloc(nmemb, size);
+#else
 	void *p = calloc(nmemb, size);
+#endif
 	if (NULL == p) {
 		fprintf(stderr,
 				"[%s] Failed to allocate %zd bytes at %s line %u: %s\n",
@@ -22,7 +40,11 @@ void *wrap_calloc(size_t nmemb, size_t size,
 
 void *wrap_malloc(size_t size,
 				  const char *file, unsigned int line, const char *func) {
+#ifdef USE_ALIGNED_MALLOC
+  void *p = align_malloc(size);
+#else
 	void *p = malloc(size);
+#endif
 	if (NULL == p) {
 		fprintf(stderr,
 				"[%s] Failed to allocate %zd bytes at %s line %u: %s\n",
