acedb6a2b489120e998ff023c870d1b266812881
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2014-03-26 16:12:34 +0100

    comma separated fastq for bwa-mem

--- fastmap.c
+++ fastmap.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
 #include "bwa.h"
 #include "bwamem.h"
 #include "kvec.h"
 #include "utils.h"
 #include "kseq.h"
+#include "utils.h"
 KSEQ_DECLARE(gzFile)
 
 extern unsigned char nst_nt4_table[256];
@@ -14,32 +17,33 @@ extern unsigned char nst_nt4_table[256];
 void *kopen(const char *fn, int *_fd);
 int kclose(void *a);
 
+
 /* all pointers needed to open and process a fastq file */
 struct FastQHandler
 	{
-	char* filename;
+	char* path;//all fastq comma-separated
+	char* file;//current fastq
 	gzFile fp;
 	int fd;
 	void* ko;
 	kseq_t *ks;
-	char* next;//start of next filename in path
-	char *last_colon;//end of next filename in path
+	int next;//start of next filename in path
 	};
 
 int main_mem(int argc, char *argv[])
 {
 	mem_opt_t *opt;
-	int i, c, n, copy_comment = 0;
+	int  i, c, n, copy_comment = 0;
 	bseq1_t *seqs;
 	bwaidx_t *idx;
 	char *rg_line = 0;
-
+	int64_t n_processed = 0;
 	struct FastQHandler fastqinput[2];
-	memset(fastqinput,2*sizeof(struct FastQHandler),0);
 	int loop_fastq_done=0;
-
+	
+	memset(fastqinput,0,2*sizeof(struct FastQHandler));
 	opt = mem_opt_init();
-	while ((c = getopt(argc, argv, "paMCSPHk:c:v:s:r:t:R:A:B:O:E:U:w:L:d:T:")) >= 0) {
+	while ((c = getopt(argc, argv, "paMCSPHk:c:v:s:r:t:R:A:B:O:E:U:w:L:d:T:Q:D:m:")) >= 0) {
 		if (c == 'k') opt->min_seed_len = atoi(optarg);
 		else if (c == 'w') opt->w = atoi(optarg);
 		else if (c == 'A') opt->a = atoi(optarg);
@@ -47,11 +51,9 @@ int main_mem(int argc, char *argv[])
 		else if (c == 'O') opt->q = atoi(optarg);
 		else if (c == 'E') opt->r = atoi(optarg);
 		else if (c == 'T') opt->T = atoi(optarg);
-		else if (c == 'L') opt->pen_clip = atoi(optarg);
 		else if (c == 'U') opt->pen_unpaired = atoi(optarg);
 		else if (c == 't') opt->n_threads = atoi(optarg), opt->n_threads = opt->n_threads > 1? opt->n_threads : 1;
 		else if (c == 'P') opt->flag |= MEM_F_NOPAIRING;
-		else if (c == 'H') opt->flag |= MEM_F_HARDCLIP;
 		else if (c == 'a') opt->flag |= MEM_F_ALL;
 		else if (c == 'p') opt->flag |= MEM_F_PE;
 		else if (c == 'M') opt->flag |= MEM_F_NO_MULTI;
@@ -60,13 +62,24 @@ int main_mem(int argc, char *argv[])
 		else if (c == 'd') opt->zdrop = atoi(optarg);
 		else if (c == 'v') bwa_verbose = atoi(optarg);
 		else if (c == 'r') opt->split_factor = atof(optarg);
+		else if (c == 'D') opt->chain_drop_ratio = atof(optarg);
+		else if (c == 'm') opt->max_matesw = atoi(optarg);
 		else if (c == 'C') copy_comment = 1;
-		else if (c == 'R') {
+		else if (c == 'Q') {
+			opt->mapQ_coef_len = atoi(optarg);
+			opt->mapQ_coef_fac = opt->mapQ_coef_len > 0? log(opt->mapQ_coef_len) : 0;
+		} else if (c == 'L') {
+			char *p;
+			opt->pen_clip5 = opt->pen_clip3 = strtol(optarg, &p, 10);
+			if (*p != 0 && ispunct(*p) && isdigit(p[1]))
+				opt->pen_clip3 = strtol(p+1, &p, 10);
+		} else if (c == 'R') {
 			if ((rg_line = bwa_set_rg(optarg)) == 0) return 1; // FIXME: memory leak
 		} else if (c == 's') opt->split_width = atoi(optarg);
+		else return 1;
 	}
 	if (opt->n_threads < 1) opt->n_threads = 1;
-	if (optind + 1 >= argc) {
+	if (optind + 1 >= argc || optind + 3 < argc) {
 		fprintf(stderr, "\n");
 		fprintf(stderr, "Usage: bwa mem [options] <idxbase> <in1.fq> [in2.fq]\n\n");
 		fprintf(stderr, "Algorithm options:\n\n");
@@ -77,13 +90,15 @@ int main_mem(int argc, char *argv[])
 		fprintf(stderr, "       -r FLOAT   look for internal seeds inside a seed longer than {-k} * FLOAT [%g]\n", opt->split_factor);
 //		fprintf(stderr, "       -s INT     look for internal seeds inside a seed with less than INT occ [%d]\n", opt->split_width);
 		fprintf(stderr, "       -c INT     skip seeds with more than INT occurrences [%d]\n", opt->max_occ);
+		fprintf(stderr, "       -D FLOAT   drop chains shorter than FLOAT fraction of the longest overlapping chain [%.2f]\n", opt->chain_drop_ratio);
+		fprintf(stderr, "       -m INT     perform at most INT rounds of mate rescues for each read [%d]\n", opt->max_matesw);
 		fprintf(stderr, "       -S         skip mate rescue\n");
 		fprintf(stderr, "       -P         skip pairing; mate rescue performed unless -S also in use\n");
 		fprintf(stderr, "       -A INT     score for a sequence match [%d]\n", opt->a);
 		fprintf(stderr, "       -B INT     penalty for a mismatch [%d]\n", opt->b);
 		fprintf(stderr, "       -O INT     gap open penalty [%d]\n", opt->q);
 		fprintf(stderr, "       -E INT     gap extension penalty; a gap of size k cost {-O} + {-E}*k [%d]\n", opt->r);
-		fprintf(stderr, "       -L INT     penalty for clipping [%d]\n", opt->pen_clip);
+		fprintf(stderr, "       -L INT     penalty for clipping [%d]\n", opt->pen_clip5);
 		fprintf(stderr, "       -U INT     penalty for an unpaired read pair [%d]\n", opt->pen_unpaired);
 		fprintf(stderr, "\nInput/output options:\n\n");
 		fprintf(stderr, "       -p         first query file consists of interleaved paired-end sequences\n");
@@ -93,7 +108,6 @@ int main_mem(int argc, char *argv[])
 		fprintf(stderr, "       -T INT     minimum score to output [%d]\n", opt->T);
 		fprintf(stderr, "       -a         output all alignments for SE or unpaired PE\n");
 		fprintf(stderr, "       -C         append FASTA/FASTQ comment to SAM output\n");
-		fprintf(stderr, "       -H         hard clipping\n");
 		fprintf(stderr, "       -M         mark shorter split hits as secondary (for Picard/GATK compatibility)\n");
 		fprintf(stderr, "\nNote: Please read the man page for detailed description of the command line and options.\n");
 		fprintf(stderr, "\n");
@@ -103,11 +117,8 @@ int main_mem(int argc, char *argv[])
 
 	bwa_fill_scmat(opt->a, opt->b, opt->mat);
 	if ((idx = bwa_idx_load(argv[optind], BWA_IDX_ALL)) == 0) return 1; // FIXME: memory leak
-
-
 	
-
-	fastqinput[0].filename = argv[optind + 1];
+	fastqinput[0].path = argv[optind + 1];
 	
 	if (optind + 2 < argc)
 		{
@@ -117,38 +128,43 @@ int main_mem(int argc, char *argv[])
 			}
 		else
 			{
-			fastqinput[1].filename = argv[optind + 2];
+			fastqinput[1].path = argv[optind + 2];
 			opt->flag |= MEM_F_PE;
 			}
 		}
 	
-	for(i=0;i< 2;++i) fastqinput[i].next = fastqinput[i].filename;
+	for(i=0;i< 2;++i) fastqinput[i].next =0;
 	
 	
 	bwa_print_sam_hdr(idx->bns, rg_line);
-	do //loop over all colon separated files
+	
+	do //loop over all comma separated files
 		{
 
 		//open each fastq 
 		for(i=0;i< 2;++i)
 			{
 			struct FastQHandler* handler=&fastqinput[i];
+			char *comma;
+			if(handler->path==NULL) continue;
+
+			handler->file=strdup(&handler->path[handler->next]);
+			comma= strchr(handler->file,',');
 			
-			if(handler->filename==NULL) continue;
-			handler->last_colon = strchr(handler->next,':');
-			if(handler->last_colon!=NULL)
+			if(comma!=NULL)
 				{
-				handler->last_colon[0]=0;
-				if( *(handler->last_colon+1)==0) loop_fastq_done=1; //empty string at the end
+				*comma=0;
+				handler->next+= 1+strlen(handler->file);
+				if( handler->path[handler->next]==0 || strcmp(&handler->path[handler->next],",")==0) loop_fastq_done=1; //empty string or single comma at the end
 				}
 			else
 				{
 				loop_fastq_done=1;
 				}
-			
-			handler->ko = kopen(handler->next, &(handler->fd));
+				
+			handler->ko = kopen(handler->file, &(handler->fd));
 			if (handler->ko == 0) {
-				if (bwa_verbose >= 1) fprintf(stderr, "[E::%s] fail to open file `%s'.\n", __func__,handler->next);
+				if (bwa_verbose >= 1) fprintf(stderr, "[E::%s] fail to open file `%s'.\n", __func__,handler->file);
 				return 1;
 				}
 			handler->fp = gzdopen(handler->fd, "r");
@@ -170,7 +186,8 @@ int main_mem(int argc, char *argv[])
 			for (i = 0; i < n; ++i) size += seqs[i].l_seq;
 			if (bwa_verbose >= 3)
 				fprintf(stderr, "[M::%s] read %d sequences (%ld bp)...\n", __func__, n, (long)size);
-			mem_process_seqs(opt, idx->bwt, idx->bns, idx->pac, n, seqs, 0);
+			mem_process_seqs(opt, idx->bwt, idx->bns, idx->pac,  n_processed,n, seqs, 0);
+			n_processed += n;
 			for (i = 0; i < n; ++i) {
 				fputs(seqs[i].sam, stdout);
 				free(seqs[i].name); free(seqs[i].comment); free(seqs[i].seq); free(seqs[i].qual); free(seqs[i].sam);
@@ -182,11 +199,11 @@ int main_mem(int argc, char *argv[])
 		for(i=0;i< 2;++i)
 			{
 			struct FastQHandler* handler=&fastqinput[i];
-			if(handler->filename==NULL) continue;
+			if(handler->path==NULL) continue;
+			free(handler->file);
 			kseq_destroy(handler->ks);
-			gzclose(handler->fp);
+			err_gzclose(handler->fp);
 			kclose(handler->ko);
-			handler->next = handler->last_colon+1;
 			}
 		
 		
@@ -215,6 +232,7 @@ int main_fastmap(int argc, char *argv[])
 			case 'p': print_seq = 1; break;
 			case 'w': min_iwidth = atoi(optarg); break;
 			case 'l': min_len = atoi(optarg); break;
+		    default: return 1;
 		}
 	}
 	if (optind + 1 >= argc) {
@@ -222,16 +240,16 @@ int main_fastmap(int argc, char *argv[])
 		return 1;
 	}
 
-	fp = gzopen(argv[optind + 1], "r");
+	fp = xzopen(argv[optind + 1], "r");
 	seq = kseq_init(fp);
-	idx = bwa_idx_load(argv[optind], BWA_IDX_BWT|BWA_IDX_BNS);
+	if ((idx = bwa_idx_load(argv[optind], BWA_IDX_BWT|BWA_IDX_BNS)) == 0) return 1;
 	itr = smem_itr_init(idx->bwt);
 	while (kseq_read(seq) >= 0) {
-		printf("SQ\t%s\t%ld", seq->name.s, seq->seq.l);
+		err_printf("SQ\t%s\t%ld", seq->name.s, seq->seq.l);
 		if (print_seq) {
-			putchar('\t');
-			puts(seq->seq.s);
-		} else putchar('\n');
+			err_putchar('\t');
+			err_puts(seq->seq.s);
+		} else err_putchar('\n');
 		for (i = 0; i < seq->seq.l; ++i)
 			seq->seq.s[i] = nst_nt4_table[(int)seq->seq.s[i]];
 		smem_set_query(itr, seq->seq.l, (uint8_t*)seq->seq.s);
@@ -239,7 +257,7 @@ int main_fastmap(int argc, char *argv[])
 			for (i = 0; i < a->n; ++i) {
 				bwtintv_t *p = &a->a[i];
 				if ((uint32_t)p->info - (p->info>>32) < min_len) continue;
-				printf("EM\t%d\t%d\t%ld", (uint32_t)(p->info>>32), (uint32_t)p->info, (long)p->x[2]);
+				err_printf("EM\t%d\t%d\t%ld", (uint32_t)(p->info>>32), (uint32_t)p->info, (long)p->x[2]);
 				if (p->x[2] <= min_iwidth) {
 					for (k = 0; k < p->x[2]; ++k) {
 						bwtint_t pos;
@@ -248,18 +266,19 @@ int main_fastmap(int argc, char *argv[])
 						pos = bns_depos(idx->bns, bwt_sa(idx->bwt, p->x[0] + k), &is_rev);
 						if (is_rev) pos -= len - 1;
 						bns_cnt_ambi(idx->bns, pos, len, &ref_id);
-						printf("\t%s:%c%ld", idx->bns->anns[ref_id].name, "+-"[is_rev], (long)(pos - idx->bns->anns[ref_id].offset) + 1);
+						err_printf("\t%s:%c%ld", idx->bns->anns[ref_id].name, "+-"[is_rev], (long)(pos - idx->bns->anns[ref_id].offset) + 1);
 					}
-				} else fputs("\t*", stdout);
-				putchar('\n');
+				} else err_puts("\t*");
+				err_putchar('\n');
 			}
 		}
-		puts("//");
+		err_puts("//");
 	}
 
 	smem_itr_destroy(itr);
 	bwa_idx_destroy(idx);
 	kseq_destroy(seq);
-	gzclose(fp);
+	err_gzclose(fp);
 	return 0;
 }
+
