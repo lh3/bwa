6ac8dd584023de02525587936c5d2b5fadd9d96b
Heng Li <lh3@me.com>
2014-05-06 16:15:14 -0400

    r754: added command msg for -h

--- fastmap.c
+++ fastmap.c
@@ -156,6 +156,7 @@ int main_mem(int argc, char *argv[])
 		fprintf(stderr, "\n");
 		fprintf(stderr, "       -v INT        verbose level: 1=error, 2=warning, 3=message, 4+=debugging [%d]\n", bwa_verbose);
 		fprintf(stderr, "       -T INT        minimum score to output [%d]\n", opt->T);
+		fprintf(stderr, "       -h INT        if #hits < INT, output all in the XA tag [%d]\n", opt->max_hits);
 		fprintf(stderr, "       -a            output all alignments for SE or unpaired PE\n");
 		fprintf(stderr, "       -C            append FASTA/FASTQ comment to SAM output\n");
 		fprintf(stderr, "       -M            mark shorter split hits as secondary\n\n");
