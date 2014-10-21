5e00d083462b0eca2be9c4e6eea0fe2d11ffa494
Heng Li <lh3@me.com>
2014-10-21 09:26:19 -0400

    r939: fixed a memory leak (issue #35)

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "utils.h"
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "0.7.10-r933-dirty"
+#define PACKAGE_VERSION "0.7.10-r939-dirty"
 #endif
 
 int bwa_fa2pac(int argc, char *argv[]);
