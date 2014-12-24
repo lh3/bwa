b9bbf2d3df55c7c5e65e13e2113de46dc19455b4
Heng Li <lh3@me.com>
2014-12-24 00:49:50 -0500

    r1036: PE ALT mapping incorrect
    No SA/XA tags!

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "utils.h"
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "0.7.11-r1034"
+#define PACKAGE_VERSION "0.7.11-r1036-dirty"
 #endif
 
 int bwa_fa2pac(int argc, char *argv[]);
