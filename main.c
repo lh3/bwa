9f26bfcc7780753129b60717ecab0ebba6f04b7c
Heng Li <lh3@me.com>
2017-10-23 13:10:17 -0400

    Released bwa-0.7.17 (r1188)

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "utils.h"
 
 #ifndef PACKAGE_VERSION
-#define PACKAGE_VERSION "0.7.16a-r1187-dirty"
+#define PACKAGE_VERSION "0.7.17-r1188"
 #endif
 
 int bwa_fa2pac(int argc, char *argv[]);
