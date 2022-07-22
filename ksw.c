c49ba444c2c7a2369f2b1aeaa6950cbf1f301790
gayoung93 <gy.lee@metisx.com>
2022-07-22 16:50:18 +0900

    add break condition to making cigar string

--- ksw.c
+++ ksw.c
@@ -590,7 +590,11 @@ int ksw_global2(int qlen, const uint8_t *query, int tlen, const uint8_t *target,
 		uint32_t *cigar = 0, tmp;
 		i = tlen - 1; k = (i + w + 1 < qlen? i + w + 1 : qlen) - 1; // (i,k) points to the last cell
 		while (i >= 0 && k >= 0) {
-			which = z[(long)i * n_col + (k - (i > w? i - w : 0))] >> (which<<1) & 3;
+			int j_beg = i > w? i - w : 0;
+			int j_end = i + w + 1 < qlen? i + w + 1 : qlen;
+			j = (k - j_beg);
+			if (j < j_beg || j >= j_end)	break;
+			which = z[(long)i * n_col + j] >> (which<<1) & 3;
 			if (which == 0)      cigar = push_cigar(&n_cigar, &m_cigar, cigar, 0, 1), --i, --k;
 			else if (which == 1) cigar = push_cigar(&n_cigar, &m_cigar, cigar, 2, 1), --i;
 			else                 cigar = push_cigar(&n_cigar, &m_cigar, cigar, 1, 1), --k;
